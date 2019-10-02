99741ae1df02cb113cfc282bf99b224a18c96ab7
massimocoletti <massimo.coletti@gmail.com>
2019-10-02 20:06:12 +0200

    introdotti buttons

--- app/app.module.ts
+++ app/app.module.ts
@@ -13,8 +13,22 @@ import { LavorazioniActComponent }
 
 import { AppComponent } from './app.component';
 import { CustomMaterialModule } from './material.module';
+
+import {
+  //MdToolbarModule,
+  //MdTabsModule,
+  MatButtonModule,
+  //MdInputModule,
+  //MdDatepickerModule,
+  //MdNativeDateModule,
+  //MdCheckboxModule,
+  //MdRadioModule
+} from '@angular/material';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, FlexLayoutModule, CustomMaterialModule ],
+  imports:      [ BrowserModule, FormsModule, FlexLayoutModule, CustomMaterialModule,
+  MatButtonModule
+   ],
   declarations: [ AppComponent , OrderheaderComponent
                   , LavorazioniComponent, LavorazioniActComponent],
   bootstrap:    [ AppComponent ]
